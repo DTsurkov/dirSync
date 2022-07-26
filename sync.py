518278dc15cb0ef28760bb1ef16cd878f6ecb0a8
DTsurkov <adduction@hotmail.com>
2022-07-26 22:14:22 +0200

    The first part of the refactoring
    add main;
    add ctr+c handling;
    add constants for directions
    add exception handling for remove items
    made code more readable

--- sync.py
+++ sync.py
@@ -1,26 +1,43 @@
-import time, argparse, queue, threading
+import time
+import argparse
+import queue
+import threading
+import sys
 import synchelper as sh
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d','-dirs', nargs='+', help='directories',required=True)
-parser.add_argument('-i','-interval', type=int, help='sync interval in seconds', default = 1)
-parser.add_argument('--verbose', help='enable verbose print', action="store_true")
 
-args = parser.parse_args()
-verboseprint = print if args.verbose else lambda *a, **k: None
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d','-dirs', nargs='+', help='directories',required=True)
+    parser.add_argument('-i','-interval', type=int, help='sync interval in seconds', default = 1)
+    parser.add_argument('--verbose', help='enable verbose print', action="store_true")
+    args = parser.parse_args()
 
-index = []
-itemsToSync = queue.Queue()
-CopyInterval = 1
-copyThread = threading.Thread(target=sh.syncItemsAsync, args=(itemsToSync,CopyInterval))
-copyThread.start()
+    index = []
+    itemsToSync = queue.Queue()
+    copyInterval = 1
+    copyThreadRun = threading.Event()
+    copyThreadRun.set()
+    copyThread = threading.Thread(target=sh.syncItemsAsync, args=(itemsToSync,copyInterval,copyThreadRun))
+    copyThread.start()
+
+    try:
+        while(1):
+            sh.enumItems(index, args.d)
+            sh.compareItems(index)
+            if args.verbose:
+                sh.printItems(index)
+            sh.getitemsToSync(itemsToSync, index)
+            sh.delRemovedItems(index)
+            time.sleep(args.i)
+    except KeyboardInterrupt:
+        print("Stop signal has been received")
+        copyThreadRun.clear()
+        print("Waiting to close copier thread")
+        copyThread.join()
+        print("Script has been stopped")
+        sys.exit(0)
 
 
-while(1):
-    sh.enumItems(index, args.d)
-    sh.compareItems(index)
-    if args.verbose:
-        sh.printItems(index)
-    sh.getItemsToSync(itemsToSync, index)
-    sh.delRemovedItems(index)
-    time.sleep(args.i)
+if __name__ == "__main__":
+    main()
